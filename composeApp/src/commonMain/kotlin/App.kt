c002e1ecf39343de7f718658ce79bb97d3a5d5bc
Leay15 <ivan_yael_gp@outlook.com>
2024-04-28 15:52:15 -0600

    Changes show Timezone for location using Dropdown list

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,19 +1,26 @@
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.datetime.Clock
+import kotlinx.datetime.IllegalTimeZoneException
 import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.LocalTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 import org.jetbrains.compose.resources.ExperimentalResourceApi
@@ -23,34 +30,81 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 import moviesappkm.composeapp.generated.resources.Res
 import moviesappkm.composeapp.generated.resources.compose_multiplatform
 
+
+data class Country(val name: String, val zone: TimeZone)
+
+fun countries() = listOf(
+    Country("Japan", TimeZone.of("Asia/Tokyo")),
+    Country("France", TimeZone.of("Europe/Paris")),
+    Country("Mexico", TimeZone.of("America/Mexico_City")),
+    Country("Indonesia", TimeZone.of("Asia/Jakarta")),
+    Country("Egypt", TimeZone.of("Africa/Cairo"))
+)
+
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+        var showCountries by remember { mutableStateOf(false) }
+        var timeAtLocation by remember { mutableStateOf("No location Selected") }
+        Column(modifier = Modifier.padding(20.dp)) {
             Text(
-                text= "Today's date is ${todaysDate()}",
-                modifier = Modifier.padding(20.dp),
-                fontSize = 24.sp,
-                textAlign = TextAlign.Center
+                timeAtLocation,
+                style = TextStyle(
+                    fontSize = 20.sp
+                ),
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth().align(Alignment.CenterHorizontally)
             )
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+            Row(modifier = Modifier.padding(start = 20.dp, top = 10.dp)) {
+                DropdownMenu(
+                    expanded = showCountries,
+                    onDismissRequest = {
+                        showCountries = false
+                    }
+                ) {
+                    countries().forEach { (name, zone) ->
+                        DropdownMenuItem(
+                            onClick = {
+                                timeAtLocation = currentTimeAt(name, zone)
+                                showCountries = false
+                            }
+                        ) {
+                            Text(name)
+                        }
+                    }
                 }
+            }
+            Button(
+                modifier = Modifier.padding(top = 10.dp),
+                onClick = {
+                    showCountries = !showCountries
+                }) {
+                Text("Select Location")
             }
         }
     }
 
 }
-fun todaysDate(): String{
+
+fun currentTimeAt(location: String, zone: TimeZone): String {
+    fun LocalTime.formatted() = "$hour:${
+        if (minute < 10) {
+            "0$minute"
+        } else minute
+    }:$second"
+
+    return try {
+        val time = Clock.System.now()
+        val localTime = time.toLocalDateTime(zone).time
+        "The time in $location is ${localTime.formatted()}"
+    } catch (ex: IllegalTimeZoneException) {
+        "Cannot Get time in Location"
+    }
+}
+
+fun todaysDate(): String {
     fun LocalDateTime.format() = toString().substringBefore('T')
     val now = Clock.System.now()
     val zone = TimeZone.currentSystemDefault()
